326583f43ce89f52eaed2015e83900a357ff3f68
catarina wor <catarinawor@gmail.com>
2017-07-28 09:35:03 -0700

    ch scn simple 3at04d2

--- admb/simtest.sh
+++ admb/simtest.sh
@@ -14,7 +14,7 @@ do
 	cd ../../mov_est/simple/
 	./lagrangian_estlog
 	
-	cd ../../../simeval/SimResult_3areas_tau04/
+	cd ../../../simeval/SimResult_3areas_tau04_delta2/
 	
 	#source(simRun.r, chdir = TRUE) | R --vanilla --slave
 	
