2186e7afe83110386e2cb1ce7a1f60f509fde5c0
catarina wor <catarinawor@gmail.com>
2017-07-28 11:01:49 -0700

    gtg 5at1 simple 3at1

--- simeval/SimResult_3areas_tau1/simRun.r
+++ simeval/SimResult_3areas_tau1/simRun.r
@@ -6,9 +6,7 @@
 #it's staurday and I'm too lazy to write a makefile
 #
 #==============================================
-setwd(getwd())
-source("../R/read.admb.R")
-
+source("../../R/read.admb.R")
 
 
 read.pin <- function(fn)
@@ -45,10 +43,10 @@ readOutput <- function(dir)
 
 
 
-	seed<-scan("../admb/seed.txt")	
+	seed<-scan("../../admb/seed.txt")	
 	file.name <- paste("simest",seed,".Rdata",sep="")
-	sims<-readOutput("../admb/")
-	setwd(getwd())
+	sims<-readOutput("../../admb/")
+	setwd("../simeval/SimResult_3areas_tau1")
 	save(sims,file=file.name)
 
 
