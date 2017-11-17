54df340a31d1d74656d21170ee1807e2d94a67da
catarina wor <catarinawor@gmail.com>
2017-11-17 14:04:29 -0800

    more angrycrab stuff 2

--- R/OM_dat/reset_seed.R
+++ R/OM_dat/reset_seed.R
@@ -8,7 +8,7 @@
 
 reset_seed<-function(DIR= "../../admb/OM/gtg/", sub_num=10){
 	#"/Users/catarinawor/Documents/Lagrangian/admb/OM/gtg/"
-	setwd(DIR)
+	#setwd(DIR)
 	myseed<-scan(file = "seed.txt")
 	write.table(myseed-sub_num, file="seed.txt", row.names = F,
             col.names = F,)
