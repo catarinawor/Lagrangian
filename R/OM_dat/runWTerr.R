f82fc9be28e6ecf962b94e0b0b10e34cebe6a1f4
catarina wor <catarinawor@gmail.com>
2017-11-17 13:48:59 -0800

    angrycrab stuff

--- R/OM_dat/runWTerr.R
+++ R/OM_dat/runWTerr.R
@@ -7,9 +7,11 @@
 args <- commandArgs(trailingOnly=TRUE)
 
 
-source("/Users/catarinawor/Documents/Lagrangian/R/OM_dat/rec_dat.R")
+#source("/Users/catarinawor/Documents/Lagrangian/R/OM_dat/rec_dat.R")
+source("rec_dat.R")
 
-sed<-scan("/Users/catarinawor/Documents/Lagrangian/admb/OM/gtg/seed.txt")
+
+sed<-scan("../../admb/OM/gtg/seed.txt")
 
 
 writeWTdat(myseed=sed)
